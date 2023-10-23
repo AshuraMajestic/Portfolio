bb4bfd2c34f87a17c39cf13feb042840432f79c7
Ashura <ashuramajestic@gmail.com>
2023-10-23 13:17:17 +0530

    portfolio

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-
+// const DB = "mongodb://127.0.0.1/Portfolio"
+const DB = process.env.MONGO_URI;
 //  Creating a database
-mongoose.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 }).then(() => {
     console.log("Connection Created");
 }).catch((err) => {
